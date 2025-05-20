6ca82e041f8df0d1ec16e1a6b7445dde0619ebe1
hwkoo111 <qkrrn0315@naver.com>
2025-05-20 17:22:57 +0900

    업로드

--- backend/src/main/java/deu/movietalk/repository/MemberFavoriteMovieRepository.java
+++ backend/src/main/java/deu/movietalk/repository/MemberFavoriteMovieRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 import java.util.Optional;
 
-public interface MemberFavoriteMovieRepository extends JpaRepository<MemberFavoriteMovie,String> {
+public interface MemberFavoriteMovieRepository extends JpaRepository<MemberFavoriteMovie, Long> {
     void deleteByMember_MemberId(String memberId); // 'member' 엔티티의 'id' 속성에 접근
     @Query("SELECT mfm FROM MemberFavoriteMovie mfm WHERE mfm.member.id = :memberId AND mfm.movie.id = :movieId")
     Optional<MemberFavoriteMovie> findByMemberIdAndMovieId(String memberId, Long movieId);
